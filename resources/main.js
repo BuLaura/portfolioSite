91819f0bacf482b3e0eaa8a6058c36bcf2d22cd9
Laura Buchwald <laura.buchwald@outlook.com>
2023-09-17 19:17:02 -0400

    Add JS

--- resources/main.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    let scrollY = window.scrollY;
-    let maxScroll = 700;
-    document.getElementsByClassName("pageTop")[0].style.opacity = ((maxScroll - scrollY)/maxScroll)/.84;
-})
